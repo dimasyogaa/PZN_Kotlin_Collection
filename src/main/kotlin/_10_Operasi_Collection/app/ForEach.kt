e6f2e0f81aac0d528d357dce3f1199cfd6f1bfeb
Yogadimas <yogadimaspambudi@gmail.com>
2023-11-17 16:48:17 +0700

    Operasi Collection - ForEach, ForEachIndexed

---
+++ src/main/kotlin/_10_Operasi_Collection/app/ForEach.kt
@@ -0,0 +1,24 @@
+package _10_Operasi_Collection.app
+
+
+import _MyHelper.MyHelper.spaceLine
+
+fun main() {
+
+    // jika menggunakan for
+    // for (value in listOf("Yoga", "Dimas", "Pambudi")) println(value)
+
+    // spaceLine()
+
+    // menngunakan extension function forEach
+    // listOf("Yoga", "Dimas", "Pambudi").forEach({ println(it) })
+    listOf("Yoga", "Dimas", "Pambudi").forEach { println(it) }
+
+    spaceLine()
+
+    mutableListOf("Yoga", "Dimas", "Pambudi").forEachIndexed { index, value ->
+        println("$index : $value")
+    }
+
+    // forEach bisa digunakan pada semua kelas turunan Iterable
+}
